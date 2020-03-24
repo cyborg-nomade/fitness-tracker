54ba870ddd7b42cbdbb698b76cbf4ac7e3dd90ec
URIEL ALEXIS FARIZELI FIORI <URIEL.FIORI@cptm.sp.gov.br>
2020-03-24 16:40:32 -0300

    finished dialog with responsivity

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { StopTrainingComponent } from './stop-training.component';
 
@@ -8,12 +8,17 @@ import { StopTrainingComponent } from './stop-training.component';
   styleUrls: ['./current-training.component.scss']
 })
 export class CurrentTrainingComponent implements OnInit {
+  @Output() traningExit = new EventEmitter();
   progress = 0;
   timer: number;
 
   constructor(private dialog: MatDialog) {}
 
   ngOnInit() {
+    this.startOrResumeProgress();
+  }
+
+  startOrResumeProgress() {
     this.timer = setInterval(() => {
       this.progress = this.progress + 5;
       if (this.progress >= 100) {
@@ -29,7 +34,11 @@ export class CurrentTrainingComponent implements OnInit {
     });
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log(result);
+      if (result) {
+        this.traningExit.emit();
+      } else {
+        this.startOrResumeProgress();
+      }
     });
   }
 }
