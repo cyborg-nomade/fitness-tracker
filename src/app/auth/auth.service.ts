f0c0dc26cfdf9b98d35c5b2c85b353fa0b801ecc
URIEL ALEXIS FARIZELI FIORI <URIEL.FIORI@cptm.sp.gov.br>
2020-03-26 17:07:42 -0300

    redirects on login and logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,10 +1,16 @@
 import { AuthData } from './auth-data.model';
 import { User } from './user.model';
-import { Subject } from 'rxjs';
 
+import { Subject } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable()
 export class AuthService {
   authChange = new Subject<boolean>();
   private user: User;
+
+  constructor(private router: Router) {}
 
   registerUser(authData: AuthData) {
     this.user = {
@@ -12,6 +18,7 @@ export class AuthService {
       userId: Math.round(Math.random() * 10000).toString()
     };
     this.authChange.next(true);
+    this.authSuccessfully();
   }
 
   login(authData: AuthData) {
@@ -20,11 +27,13 @@ export class AuthService {
       userId: Math.round(Math.random() * 10000).toString()
     };
     this.authChange.next(true);
+    this.authSuccessfully();
   }
 
   logout() {
     this.user = null;
     this.authChange.next(false);
+    this.router.navigate(['/login']);
   }
 
   getUser() {
@@ -33,5 +42,9 @@ export class AuthService {
 
   isAuth() {
     return this.user != null;
+  }
+
+  private authSuccessfully() {
+    this.router.navigate(['/training']);
   }
 }
