e68ad9d3ed9ab84012f6fff23d901d1c4af1a564
URIEL ALEXIS FARIZELI FIORI <URIEL.FIORI@cptm.sp.gov.br>
2020-04-28 17:22:40 -0300

    auth routes in the aouth-routing module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,10 +5,11 @@ import { SharedModule } from './../shared/shared.module';
 
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
   declarations: [SignupComponent, LoginComponent],
-  imports: [AngularFireAuthModule, SharedModule],
+  imports: [AngularFireAuthModule, SharedModule, AuthRoutingModule],
   exports: [],
   providers: [],
 })
